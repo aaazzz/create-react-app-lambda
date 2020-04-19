de755a4abaee14c7d777a11b5f02ab9c6e5ee69a
Matsui Akira <a.matsui0725@gmail.com>
2020-04-19 12:55:54 +0700

    work successfully

--- src/lambda/slack.js
+++ src/lambda/slack.js
@@ -1,25 +1,26 @@
-const fetch = require('node-fetch')
-const slackURL = process.env.SLACK_WEBHOOK_URL
-export function handler(event, context, callback) {
+import axios from 'axios'
+const slackURL = process.env.SLACK_WEBHOOK_URL || 'https://hooks.slack.com/services/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'
+console.log(slackURL)
+export async function handler(event, context, callback) {
   if (event.httpMethod !== 'POST') {
     return callback(null, {
       statusCode: 410,
       body: 'Unsupported Request Method'
     })
   }
+  const data = event.body
   try {
-    const payload = JSON.parse(event.body)
-    fetch(slackURL, {
-      method: 'POST',
-      body: JSON.stringify({ text: payload.text })
-    })
-      .then(() => {
-        callback(null, { statusCode: 204 })
-      })
-      .catch(e => {
-        callback(null, { statusCode: 500, body: 'Internal Server Error: ' + e })
-      })
-  } catch (e) {
-    callback(null, { statusCode: 500, body: 'Internal Server Error: ' + e })
+    const response = await axios.post(slackURL, data)
+    console.log(response)
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ msg: data.joke })
+    }
+  } catch (err) {
+    console.log(err) // output to netlify function log
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
+    }
   }
 }
